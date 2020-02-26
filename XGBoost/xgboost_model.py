18166427f443d631276965b0dfef31b387a27d1a
David Wang <aznaznorange@gmail.com>
2020-02-25 17:44:53 -0700

    added k-fold validation to xgboost models

--- XGBoost/xgboost_model.py
+++ XGBoost/xgboost_model.py
@@ -1,7 +1,7 @@
 import xgboost as xgb
 from lib.crxXGBoost import crx_data
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import accuracy_score
 
 # import pandas as pd
@@ -43,32 +43,49 @@ def main():
     data_y = np_data[:, -1]
 
     print("Model 1 (all identifiers)")
-    model(data_x, data_y)
+    modelKValidation(data_x, data_y)
 
     print("Model 2 (top 2 most influential IDs by lasso")
     data_x2 = data_x[:, 8:10]
-    model(data_x2, data_y)
+    modelKValidation(data_x2, data_y)
 
     print("Model 3 (top 5 most influential IDs by lasso)")
     data_x3 = data_x[:, 6:11]
-    model(data_x3, data_y)
+    modelKValidation(data_x3, data_y)
 
     print("Model 4 (employed, credit, income)")
     data_x4 = data_x[:, [9, 10, 14]]
-    model(data_x4, data_y)
+    modelKValidation(data_x4, data_y)
 
-def model(data_x, data_y):
-    x_train, x_test, y_train, y_test = train_test_split(data_x,
-                                                        data_y,
-                                                        test_size=0.25,
-                                                        random_state=7)
+# def model(data_x, data_y):
+#     x_train, x_test, y_train, y_test = train_test_split(data_x,
+#                                                         data_y,
+#                                                         test_size=0.25,
+#                                                         random_state=7)
+#
+#     xg_model = xgb.XGBClassifier()
+#     xg_model.fit(x_train, y_train)
+#
+#     y_pred = [round(value) for value in xg_model.predict(x_test)]
+#
+#     accuracy = accuracy_score(y_test, y_pred)
+#     print("Accuracy: %.2f%%" % (accuracy * 100.0))
 
+def modelKValidation(data_x, data_y):
+    splits = 5
+    kf = KFold(n_splits=splits, random_state=7, shuffle=True)
     xg_model = xgb.XGBClassifier()
-    xg_model.fit(x_train, y_train)
+    totalAcc = 0.0
 
-    y_pred = [round(value) for value in xg_model.predict(x_test)]
+    for train, test in kf.split(data_x):
+        x_train, x_test = data_x[train], data_x[test]
+        y_train, y_test = data_y[train], data_y[test]
+        xg_model.fit(x_train, y_train)
+        y_pred = [round(value) for value in xg_model.predict(x_test)]
+        accuracy = accuracy_score(y_test, y_pred)
+        totalAcc = totalAcc + accuracy
 
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    averageAcc = totalAcc/splits
+    print("Accuracy: %.2f%%" % (averageAcc * 100.0))
 
 main()
